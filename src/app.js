43f9de869ffe4f9e48a28e4c6ef5523fac8ef900
Alex Sky <alex.sky23@yandex.ru>
2022-05-18 23:04:10 +0300

    first commit

--- src/app.js
+++ src/app.js
@@ -1,23 +1,21 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/style.css";
-//import taskFieldTemplate from "./templates/taskField.html";
+import taskFieldTemplate from "./templates/taskField.html";
 import noAccessTemplate from "./templates/noAccess.html";
 import { User } from "./models/User";
 import { generateTestUser } from "./utils";
 import { State } from "./state";
 import { authUser } from "./services/auth";
-import { btn_LogOff } from "./services/btn_logOff"; 
-import { user_menu_f1 } from "./models/user_menu_f1";
+import { btn_LogOff } from "./services/btn_logOff";
+
 //добавление доски
-//import laptop1_con from "./templates/laptop1.html";
-import laptop1_con from "./templates/frame1.html";
+import { user_menu_f1 } from "./models/user_menu_f1";
 export const appState = new State();
 
 const loginForm = document.querySelector("#app-login-form");
 const btnLogOn = document.querySelector("#app-login-btn");
 const btnLogOff = document.querySelector("#app-logout-btn");
 const logoff = btn_LogOff();
-const btn_user_menu_f1 = user_menu_f1();
 
 generateTestUser(User);
 
@@ -30,10 +28,7 @@ loginForm.addEventListener("submit", function (e) {
   let fieldHTMLContent = authUser(login, password);
 
   if(fieldHTMLContent){
-   // fieldHTMLContent = taskFieldTemplate;
-   fieldHTMLContent = laptop1_con;
-   btn_user_menu_f1;
-
+   fieldHTMLContent = taskFieldTemplate;
     btnLogOn.style.display = "none";
     btnLogOff.style.display = "inline";
     logoff; //перезагрузка страницы для сброса авторизации
@@ -44,4 +39,7 @@ loginForm.addEventListener("submit", function (e) {
   };
 
   document.querySelector("#content").innerHTML = fieldHTMLContent;
+
+  const btn_user_menu_f1 = user_menu_f1();
+  btn_user_menu_f1;
 });
--- src/models/user_menu_f1.js
+++ src/models/user_menu_f1.js
@@ -1,15 +1,22 @@
 export const user_menu_f1 = function () {
-const btn = document.getElementById("user-menu_f1");
-const div = document.getElementById("galka_f1");
-var marker = 0;
-
-btn.addEventListener("submit", function () {
-    alert('galka1');
+  const btn = document.getElementById("user-menu_f1");
+  const div = document.getElementById("galka_f1");
+  const u_m = document.getElementById("menu_user");
+ 
+  let marker = 1;
+  btn.addEventListener("click", function () {
     if (marker % 2 === 0) {
       div.innerHTML = `<img id="vector1" src="/src/img/galka.png" alt="menu1">`;
+      u_m.innerHTML = "";
     }
     else {
-      div.innerHTML = `<img id="vector1" src="/src/img/galka1.png" alt="menu1">`;
+      div.innerHTML = `<img id="vector2" src="/src/img/galka1.png" alt="menu1">`;
+      u_m.innerHTML = `
+      <img id="Rectangle" src="/src/img/Rectangle 27.png" alt="romb">
+      <div id="u-m_blok-txt">
+      <p id="text_m-u">Profile</p>
+      <p id="text_m-u">Log Out</p>
+      `;
     };
     marker++;
   });
